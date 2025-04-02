b23993d97be87cb519baaebdfcdba2f0c994d81d
eagereconomist <myeagereconomist@gmail.com>
2025-04-01 17:49:15 -0700

    Added necessary packages for data preparation

---
+++ tennis_racquet_analysis/data-prep/data-preparation.py
@@ -0,0 +1,8 @@
+# Install necessary packages
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+from scipy.cluster.hierarchy import dendrogram
+from sklearn.cluster import AgglomerativeClustering
+
