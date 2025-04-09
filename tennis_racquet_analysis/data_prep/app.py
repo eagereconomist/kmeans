94c87b43bae94e4fa605caa3cf57996121dd2f48
eagereconomist <myeagereconomist@gmail.com>
2025-04-08 17:37:45 -0700

    Added data processing functions in module.py and added processing pipes in app.py

--- tennis_racquet_analysis/data_prep/app.py
+++ tennis_racquet_analysis/data_prep/app.py
@@ -15,5 +15,26 @@ def main():
     print("Data preprocessing complete")
 
 
+normalized_data = module.preprocessed_data.pipe(module.apply_normalizer).pipe(
+    module.write_csv, "processed", "normalized"
+)
+
+standardized_data = module.preprocessed_data.pipe(module.apply_standardization).pipe(
+    module.write_csv, "processed", "standardized"
+)
+
+minmax_data = module.preprocessed_data.pipe(module.apply_minmax).pipe(
+    module.write_csv, "processed", "minmax"
+)
+
+log_scale_data = module.preprocessed_data.pipe(module.log_transform).pipe(
+    module.write_csv, "processed", "log_scale"
+)
+
+yeo_johnson_data = module.preprocessed_data.pipe(module.yeo_johnson).pipe(
+    module.write_csv, "processed", "yeo_johnson"
+)
+
+
 if __name__ == "__main__":
     main()
--- tennis_racquet_analysis/data_prep/module.py
+++ tennis_racquet_analysis/data_prep/module.py
@@ -2,10 +2,11 @@ import os
 import pandas as pd
 import numpy as np
 from sklearn import preprocessing
+from sklearn.preprocessing import Normalizer, StandardScaler, MinMaxScaler, PowerTransformer
+from sklearn.preprocessing import FunctionTransformer
 
 
 def load_data():
-    """Load the csv file and return a DataFrame"""
     data_path = os.path.join("data", "raw", "tennis_racquets.csv")
     print("Looking for file at:", data_path)
     if os.path.exists(data_path):
@@ -36,3 +37,33 @@ def write_csv(dataframe, subfolder, file_label):
     dataframe.to_csv(file_path, index=False)
     print(f"csv written to {file_path}")
     return dataframe
+
+
+def apply_normalizer(dataframe):
+    scaler = Normalizer()
+    scaled = scaler.fit_transform(dataframe)
+    return pd.DataFrame(scaled, columns=dataframe.columns)
+
+
+def apply_standardization(dataframe):
+    scaler = StandardScaler()
+    scaled = scaler.fit_transform(dataframe)
+    return pd.DataFrame(scaled, columns=dataframe.columns)
+
+
+def apply_minmax(dataframe):
+    scaler = MinMaxScaler()
+    scaled = scaler.fit_transform(dataframe)
+    return pd.DataFrame(scaled, columns=dataframe.columns)
+
+
+def log_transform(dataframe):
+    scaler = FunctionTransformer(np.log1p, validate=True)
+    scaled = scaler.fit_transform(dataframe)
+    return pd.DataFrame(scaled, columns=dataframe.columns)
+
+
+def yeo_johnson(dataframe):
+    scaler = preprocessing.PowerTransformer(method="yeo_johnson", standardize=True)
+    scaled = scaler.fit_transform(dataframe)
+    return pd.DataFrame(scaled, columns=dataframe.columns)
