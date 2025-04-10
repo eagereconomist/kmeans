c448c721ba80f6f7d7c51b8d702ceebe2cb7d027
eagereconomist <myeagereconomist@gmail.com>
2025-04-10 15:50:55 -0700

    Refactor: Move write_csv and data scaling functions from module.py to new file processing_utils.py for improved modularity

--- tennis_racquet_analysis/processing_utils.py
+++ tennis_racquet_analysis/processing_utils.py
@@ -1,35 +1,10 @@
-import os
+from pathlib import Path
 import pandas as pd
+from loguru import logger
 import numpy as np
 from sklearn import preprocessing
 from sklearn.preprocessing import Normalizer, StandardScaler, MinMaxScaler
 from sklearn.preprocessing import FunctionTransformer
-
-
-def load_data():
-    data_path = os.path.join("data", "raw", "tennis_racquets.csv")
-    print("Looking for file at:", data_path)
-    if os.path.exists(data_path):
-        df = pd.read_csv(data_path)
-        print("Data loaded successfully!")
-        print(df.head(10))
-        return df
-    else:
-        raise FileNotFoundError(f"File not found. Please check your path: {data_path}")
-
-
-def drop_column(dataframe, column):
-    return dataframe.drop(columns=[column])
-
-
-def rename_column(dataframe, column):
-    new_column = column.replace(".", "")
-    return dataframe.rename(columns={column: new_column})
-
-
-def squared(dataframe, column):
-    dataframe[f"{column}_sq"] = dataframe[column] ** 2
-    return dataframe
 
 
 def write_csv(dataframe, subfolder, file_label):
