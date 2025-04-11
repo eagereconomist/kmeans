893a270709db53ff3a894d014467c335f4968dcc
eagereconomist <myeagereconomist@gmail.com>
2025-04-10 23:24:26 -0700

    preprocessing.py: Removed unneeded package import

--- tennis_racquet_analysis/preprocessing.py
+++ tennis_racquet_analysis/preprocessing.py
@@ -2,8 +2,6 @@ from pathlib import Path
 from loguru import logger
 from tqdm import tqdm
 import typer
-import pandas as pd
-
 from tennis_racquet_analysis.config import RAW_DATA_DIR, INTERIM_DATA_DIR
 from tennis_racquet_analysis.preprocessing_utils import load_data, drop_column, rename_column
 
