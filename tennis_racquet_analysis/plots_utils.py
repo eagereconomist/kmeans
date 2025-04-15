2e1fbd36700b8ab89b351d368cb9d166004691e5
eagereconomist <myeagereconomist@gmail.com>
2025-04-15 16:40:49 -0700

    plots_utils.py: Edited file path

--- tennis_racquet_analysis/plots_utils.py
+++ tennis_racquet_analysis/plots_utils.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 def histogram(dir_label: str, file_label: str, output_path: str, x_axis: str, num_bins: int = 30):
     file_path = INTERIM_DATA_DIR / f"tennis_racquets_{file_label}.csv"
-    output_path = FIGURES_DIR
+    output_path = FIGURES_DIR / f"{x_axis}_{file_label}_histogram"
     df = pd.read_csv(file_path)
     if x_axis not in df.columns:
         raise ValueError(f"Column '{x_axis}' not found in the file: {file_path}")
