24126f352db3ab1d8c44c2863c0405f4d63d601d
eagereconomist <myeagereconomist@gmail.com>
2025-04-15 17:58:54 -0700

    plots_utils.py: Edited file_path

--- tennis_racquet_analysis/plots_utils.py
+++ tennis_racquet_analysis/plots_utils.py
@@ -1,10 +1,15 @@
 import pandas as pd
-from tennis_racquet_analysis.config import FIGURES_DIR, INTERIM_DATA_DIR, PROCESSED_DATA_DIR  # noqa: F401
+from tennis_racquet_analysis.config import (
+    FIGURES_DIR,
+    DATA_DIR,
+    # INTERIM_DATA_DIR,
+    # PROCESSED_DATA_DIR,
+)  # noqa: F401
 import matplotlib.pyplot as plt
 
 
 def histogram(dir_label: str, file_label: str, output_path: str, x_axis: str, num_bins: int):
-    file_path = INTERIM_DATA_DIR / f"tennis_racquets_{file_label}.csv"
+    file_path = DATA_DIR / dir_label / f"tennis_racquets_{file_label}.csv"
     output_path = FIGURES_DIR / f"{x_axis}_{file_label}_hist"
     df = pd.read_csv(file_path)
     if x_axis not in df.columns:
