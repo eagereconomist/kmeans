4206804ec48e465b21f9fa177c156eaa12bdf6b3
eagereconomist <myeagereconomist@gmail.com>
2025-04-17 00:13:43 -0700

    data_ingestion.py: Refactored hard-coded input path to a modular input file

--- tennis_racquet_analysis/data_ingestion.py
+++ tennis_racquet_analysis/data_ingestion.py
@@ -1,20 +1,33 @@
-from pathlib import Path
-from loguru import logger
+# from pathlib import Path
 import typer
-from tennis_racquet_analysis.config import RAW_DATA_DIR
+from tqdm import tqdm
+from loguru import logger
+from tennis_racquet_analysis.config import DATA_DIR
 from tennis_racquet_analysis.preprocessing_utils import load_data
 
 app = typer.Typer()
 
 
 @app.command()
-def main(input_path: Path = RAW_DATA_DIR / "tennis_racquets.csv"):
-    logger.info("Loading raw dataset...")
-    df = load_data(input_path)
-    logger.info(f"Type of loaded data: {type(df)}")
-    logger.info(f"Data shape: {df.shape}")
+def main(
+    dir_label: str,
+    input_file: str,
+):
+    logger.info("Starting Data Ingestion Pipeline")
+    pbar = tqdm(total=3, desc="Data Ingestion", ncols=80)
+    pbar.set_description("Resolving Input File Path")
+    path = DATA_DIR / dir_label / input_file
+    pbar.update(1)
+    pbar.set_description("Reading File into DataFrame")
+    df = load_data(path)
+    pbar.update(2)
+    pbar.set_description("Finalizing Data Ingestion")
     typer.echo(df.head())
-    logger.success("Dataset successfully loaded!")
+    logger.info(f"Ingested DataFrame Type: {type(df)}")
+    logger.info(f"DataFrame Dimensions: {df.shape}")
+    pbar.update(3)
+    pbar.close()
+    logger.success(f"Completed Data Ingestion from {path}")
     return df
 
 
