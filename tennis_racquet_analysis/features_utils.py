539909561dbb48b4e309e931a6042b253231ea71
eagereconomist <myeagereconomist@gmail.com>
2025-04-29 16:29:56 -0700

    features_utils.py: Added 'apply_interaction' function for pairwise feature interactions

--- tennis_racquet_analysis/features_utils.py
+++ tennis_racquet_analysis/features_utils.py
@@ -4,3 +4,8 @@ import pandas as pd
 def squared(dataframe: pd.DataFrame, column: str) -> pd.DataFrame:
     dataframe[f"{column}_sq"] = dataframe[column] ** 2
     return dataframe
+
+
+def apply_interaction(dataframe: pd.DataFrame, column_1: str, column_2: str) -> pd.DataFrame:
+    dataframe[f"{column_1}_x_{column_2}"] = dataframe[column_1] * dataframe[column_2]
+    return dataframe
