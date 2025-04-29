a0062c3abcc39b5867f744c53868b344084dfb7c
eagereconomist <myeagereconomist@gmail.com>
2025-04-29 16:04:51 -0700

    features_utils.py: Added type hints to 'squared' function

--- tennis_racquet_analysis/features_utils.py
+++ tennis_racquet_analysis/features_utils.py
@@ -1,3 +1,6 @@
-def squared(dataframe, column):
+import pandas as pd
+
+
+def squared(dataframe: pd.DataFrame, column: str) -> pd.DataFrame:
     dataframe[f"{column}_sq"] = dataframe[column] ** 2
     return dataframe
