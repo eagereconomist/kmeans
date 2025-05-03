ead7265e4999b03477a1672f239d12d31aa47609
eagereconomist <myeagereconomist@gmail.com>
2025-05-02 22:37:07 -0700

    Added 'check_iqr_outliers' function

--- tennis_racquet_analysis/preprocessing_utils.py
+++ tennis_racquet_analysis/preprocessing_utils.py
@@ -13,6 +13,17 @@ def load_data(input_path: Path) -> pd.DataFrame:
         raise FileNotFoundError(f"File not found. Please check your path: {input_path}")
 
 
+def check_iqr_outliers(df: pd.DataFrame) -> pd.DataFrame:
+    q1 = df.quantile(0.25)
+    q3 = df.quantile(0.75)
+    iqr = q3 - q1
+    lower_lim = q1 - 1.5 * iqr
+    upper_lim = q3 + 1.5 * iqr
+    outlier_mask = (df < lower_lim) | (df > upper_lim)
+    iqr_outliers = df.where(outlier_mask).stack()
+    return iqr_outliers
+
+
 def drop_column(df: pd.DataFrame, column: str) -> pd.DataFrame:
     return df.drop(columns=[column])
 
