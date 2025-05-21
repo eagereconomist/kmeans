6e666429e0c8b529602b0dd6ca63ad1f8895e9f8
eagereconomist <myeagereconomist@gmail.com>
2025-05-20 22:47:19 -0700

    Added the ability to compute and export PCA scores based on the number of features to 'compute_pca_summary' helper function

--- tennis_racquet_analysis/preprocessing_utils.py
+++ tennis_racquet_analysis/preprocessing_utils.py
@@ -39,12 +39,16 @@ def compute_pca_summary(
         feature_columns = [c for c in all_numueric_cols if c != hue_column]
     X = df[feature_columns].values
     pca = PCA(n_components=n_components, random_state=random_state).fit(X)
+    scores_array = pca.transform(X)
+    score_labels = [f"PC{i + 1}" for i in range(scores_array.shape[1])]
     pc_labels = [f"PC{i + 1}" for i in range(pca.components_.shape[0])]
     loadings = pd.DataFrame(pca.components_, index=pc_labels, columns=feature_columns)
+    scores = pd.DataFrame(scores_array, index=df.index, columns=score_labels)
     pve = pd.Series(pca.explained_variance_ratio_, index=pc_labels, name="prop_var")
     cpve = pd.Series(pve.cumsum(), index=pc_labels, name="cumulative_prop_var")
     return {
         "loadings": loadings,
+        "scores": scores,
         "pve": pve,
         "cpve": cpve,
     }
