9acd0ad953ff943daeb5dafd1f43401bb5d28d68
eagereconomist <myeagereconomist@gmail.com>
2025-05-20 17:31:43 -0700

    Changed 'input_dir' parameter for 'plot_scree' CLI

--- tennis_racquet_analysis/plots.py
+++ tennis_racquet_analysis/plots.py
@@ -561,13 +561,10 @@ def batch_cluster_plot(
 def plot_scree(
     input_file: str = typer.Argument(..., help="csv file."),
     input_dir: Path = typer.Option(
-        PROCESSED_DATA_DIR,
-        "--input_dir",
+        "processed",
+        "--input-dir",
         "-d",
-        exists=True,
-        dir_okay=True,
-        file_okay=True,
-        help="Directory where feature-engineered files live.",
+        help="Sub-folder under data/ (e.g. external, interim, processed, raw), where the input file lives.",
     ),
     output_dir: Path = typer.Option(
         FIGURES_DIR,
