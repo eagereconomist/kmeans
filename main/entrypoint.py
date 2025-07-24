db29316998a8640188efb9f29545ce50df3b9126
eagereconomist <myeagereconomist@gmail.com>
2025-07-23 21:47:40 -0700

    Refactored entrypoint.py

--- main/entrypoint.py
+++ main/entrypoint.py
@@ -2,17 +2,17 @@ import typer
 from kmflow.cli import (
     wrangle,
     kmeans,
-    pca,
     cluster_prep,
     process,
     plots,
     evaluation,
 )
+from kmflow.cli.pca import run_pca
 
 app = typer.Typer()
 app.add_typer(wrangle.app, name="wrangle")
 app.add_typer(kmeans.app, name="kmeans")
-app.add_typer(pca.app, name="pca")
+app.command("pca")(run_pca)
 app.add_typer(process.app, name="process")
 app.add_typer(plots.app, name="plots")
 app.add_typer(evaluation.app, name="evaluate")
